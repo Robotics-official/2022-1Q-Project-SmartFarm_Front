8de3df8db547e41239d048be1c66aa7b3aa6b5dc
SW Baek <44691277+SWtheWhite@users.noreply.github.com>
2022-02-08 17:53:02 +0900

    Add files via upload

--- Arduino/pump/pump.ino
+++ Arduino/pump/pump.ino
@@ -1,22 +1,27 @@
-int R = 2;
+int Watering = 2;
 
 void setup()
 {
  Serial.begin(9600); // serial communication reset
- pinMode(R, OUTPUT);
- digitalWrite(R, LOW);
+ pinMode(Watering, OUTPUT);
+ digitalWrite(Watering, LOW);
 }
 
 void loop()
 {
+  fun_Watering();
+}
+
+void fun_Watering()
+{
   Serial.println(" ** Watering System ReStart ** ");
-  digitalWrite(R, LOW); // relay off + common situation
+  digitalWrite(Watering, LOW); // relay off + common situation
   while (1) {
     if (Serial.available()) {
       String inString = Serial.readStringUntil('\n');
       Serial.println(inString);
       if (inString== "WATER_ON") {
-        digitalWrite(R, HIGH); // relay on + watering start
+        digitalWrite(Watering, HIGH); // relay on + watering start
         delay(5000); //(for 5 Second)
         Serial.println( " ** Watering is Finished ** ");
         break;
